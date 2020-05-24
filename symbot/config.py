9596e119a6caa07b727ae7d696f66e3f52da8e78
tobi208 <tobias96.home@googlemail.com>
2020-05-24 02:20:13 +0200

    fixing config tracking

--- symbot/config.py
+++
@@ -1,9 +0,0 @@
-# connection credentials
-server = 'irc.chat.twitch.tv'
-port = 6667
-token = 'oauth:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-nick = 'toms_bot'
-channel = '#tom'
-
-# prefix identifying commands
-prefix = '!'
