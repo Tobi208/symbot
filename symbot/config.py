036cb2659c00894eb1d44f9f50955b24c07bb535
tobi208 <tobias96.home@googlemail.com>
2020-05-29 18:51:06 +0200

    dummy config, data

---
+++ symbot/config.py
@@ -0,0 +1,9 @@
+# server connection
+server = 'irc.chat.twitch.tv'
+port = 6667
+
+# connection credentials
+# get your token on https://twitchapps.com/tmi/
+channel = 'broadcaster username'
+nick = 'bot username'
+token = 'oauth:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
